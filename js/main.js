2669b8cc75bf268f88ed24b869dc97f200d620e0
Sam Martinez & Mike Chastain <student@epicodus.com>
2015-09-14 09:50:09 -0700

    add JQuery to main.js page

--- js/main.js
+++ js/main.js
@@ -1,61 +1,23 @@
-var findReplace = function(w_find,w_replace)
-    { w_find =  "\\b" + w_find + "\\b";
-        var re =  new RegExp (w_find,"gi");
-        var str = $("#text_to_work_on").text();
-        var newstr = str.replace(re, w_replace);
-        //console.log(newstr);  // oranges are round, and oranges are juicy.
-        return newstr;
-    }
 
 $(document).ready(function() {
-    $("form#findReplace").submit(function(event){
-        var find_word = $('#word_find').val();
-        var replace_word = $('#word_replace').val();
-        var replaced = findReplace(find_word,replace_word);
-        console.log(replaced);
-        $("#text_to_work_on").text(replaced);
-
+    $("form#new-contact").submit(function(event) {
         event.preventDefault();
+
+        var inputtedFirstName = $("input#new-first-name").val();
+        var inputtedLastName = $("input#new-last-name").val();
+        var inputtedAddress = $("input#new-address").val();
+
+        var newContact = {
+                firstName: inputtedFirstName,
+                lastName: inputtedLastName,
+                address: inputtedAddress
+            };
+
+
+        $("ul#contacts").append( "<li><span class = 'contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>" )
+
+        $("input#new-first-name").val("");
+        $("input#new-last-name").val("");
+        $("input#new-address").val("");    
     });
 });
-
-//was tricky to implement with keyup due to not caching of original text and
-//replacing that in subsequent alterations
-
-// $('#word_replace').keyup(function(){
-//
-//     var find_word = $('#word_find').val();
-//     var replace_word = $('#word_replace').val();
-//     var replaced = findReplace(find_word,replace_word);
-//
-//     $("#text_to_work_on").text(replaced);
-//
-//     $('#word_find').val(replace_word);
-//
-//         console.log($('#word_find').val());
-//
-// })
-
-
-// var scrapeSite = function (theURL) {
-//
-// var retRes = $.ajax({
-//     url: theURL ,
-//     xhrFields:{
-//         withCredentials: true
-//             },
-//     type: 'GET',
-//     dataType: 'jsonp',
-//     jsonpCallback: 'callback',
-//
-//     success: function(json) {
-//         console.log(json);
-//         var headline = $(res.responseText).find('p').text();
-//         alert("Success");
-//     },
-//     error: function(){console.log( 'daum!');},
-//     jsonp: 'json'
-// });
-//
-// return retRes;
-// }
